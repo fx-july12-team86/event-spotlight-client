b0b7c896413f64657b097d227bd815e390d13a04
boytrend0108 <boytrend@gmail.com>
2024-07-13 15:21:11 +0300

    add: configs

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default [
+  includeIgnoreFile(gitignorePath),
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-closing-bracket-location': ['warn', 'line-aligned'], // Ensures closing JSX tags are on separate lines
+      'react/jsx-closing-tag-location': ['warn', 'line-aligned'], // Ensures closing JSX tags are on separate lines
+      'react/jsx-newline': ['error'],
+    },
+  },
+
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
---
+++ src/app/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Всім привіт!!!</h1>;
+}
+
+export default App;
