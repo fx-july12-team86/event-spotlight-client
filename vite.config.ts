a9b3cf8139d957de3d1922220bc3f1d03f23f357
boytrend0108 <boytrend@gmail.com>
2024-07-13 12:23:37 +0300

    add: gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/fx-july12-team86/event-spotlight-clien/",
   plugins: [react()],
 })
