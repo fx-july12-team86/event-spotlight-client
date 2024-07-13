3b1c7472fad956bd544b651260e0751437b00919
boytrend0108 <boytrend@gmail.com>
2024-07-13 12:25:15 +0300

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/fx-july12-team86/event-spotlight-clien/",
+  base: "/event-spotlight-client/",
   plugins: [react()],
 })
