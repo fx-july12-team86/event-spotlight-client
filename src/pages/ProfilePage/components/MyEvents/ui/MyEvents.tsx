46040c769d2f1a84b9e7d82fffc296841b6c5f6c
boytrend0108 <boytrend@gmail.com>
2024-08-08 17:58:35 +0300

    create: my events component

--- src/pages/ProfilePage/components/MyEvents/ui/MyEvents.tsx
+++ src/pages/ProfilePage/components/MyEvents/ui/MyEvents.tsx
@@ -1,9 +1,84 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import cn from 'classnames';
+
 import './MyEvents.scss';
+import { ProductList } from '../../../../../widgets/ProductList';
+import { EventType } from '../../../../../entities/Event/types';
+
+type TabType = 'active' | 'suspended' | 'moderate' | 'archive';
+
+const TABS: { id: TabType; title: string }[] = [
+  { id: 'active', title: 'Активні' },
+  { id: 'suspended', title: 'ПРИЗУПИНЕНІ' },
+  { id: 'moderate', title: 'МОДЕРАЦІЇ' },
+  { id: 'archive', title: 'АРХІВ' },
+];
+const eventsToр: EventType[] = [
+  {
+    id: 1,
+    category: 'Майстер-класс',
+    favorite: false,
+    title: 'Майстер-клас з миловаріння',
+    img: 'slider/product_1.webp',
+    options: {
+      date: '01/01.2024',
+      time: '13-00',
+      address: 'Art studio “Lila”, Київ',
+      price: 350,
+      city: 'Київ',
+    },
+  },
+  {
+    id: 2,
+    category: 'Майстер-класс',
+    favorite: false,
+    title: 'Майстер-клас з миловаріння',
+    img: 'slider/product_1.webp',
+    options: {
+      date: '01/01.2024',
+      time: '13-00',
+      address: 'Art studio “Lila”, Київ',
+      price: 350,
+      city: 'Київ',
+    },
+  },
+];
 
 export const MyEvents = () => {
+  const [tab, setTab] = useState<TabType>('active');
+
   return (
     <div className="MyEvents">
-      <h1>MyEvents</h1>
+      <nav className="MyEvents__breadcrumbs">
+        <Link to="/" className="MyEvents__breadcrumbs-item">
+          Мій профіль
+        </Link>
+
+        <button className="MyEvents__breadcrumbs-item">Мої події</button>
+      </nav>
+
+      <h1 className="MyEvents__title">Мої події</h1>
+
+      <main className="MyEvents__main">
+        <nav className="MyEvents__tabs">
+          {TABS.map((t) => (
+            <p
+              key={t.id}
+              className={cn('MyEvents__tabs-item', {
+                'MyEvents__tabs-item--active': t.id === tab,
+              })}
+              onClick={() => setTab(t.id)}
+            >
+              {t.title}
+            </p>
+          ))}
+        </nav>
+
+        <div className="Favorite__list">
+          <ProductList events={eventsToр} addButton={true} />
+        </div>
+      </main>
     </div>
   );
 };
--- src/widgets/ProductList/ui/ProductList.tsx
+++ src/widgets/ProductList/ui/ProductList.tsx
@@ -3,12 +3,14 @@ import { Fragment } from 'react/jsx-runtime';
 import './ProductList.scss';
 import { Event } from '../../../entities/Event';
 import { EventType } from '../../../entities/Event/types';
+import { Link } from 'react-router-dom';
 
 type Props = {
   events: EventType[];
+  addButton?: boolean;
 };
 
-export const ProductList: React.FC<Props> = ({ events }) => {
+export const ProductList: React.FC<Props> = ({ events, addButton = false }) => {
   return (
     <ul className="ProductList">
       {events.map((event) => (
@@ -16,6 +18,14 @@ export const ProductList: React.FC<Props> = ({ events }) => {
           <Event event={event} />
         </Fragment>
       ))}
+
+      {addButton && (
+        <li className="ProductList__extra">
+          <Link to="/profile/add-event" className="ProductList__extra-btn">
+            <p className="ProductList__extra-text"> Додати подію</p>
+          </Link>
+        </li>
+      )}
     </ul>
   );
 };
